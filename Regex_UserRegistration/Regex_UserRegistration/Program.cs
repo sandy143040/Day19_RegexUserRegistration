a3f4d0a5c1b1319ca182e7d1ee35d89acdac7c0f
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-11 04:19:05 +0530

    [Sandeep] Add : Validate First Name

---
+++ Regex_UserRegistration/Regex_UserRegistration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nPlease choose the below option: ");
+                Console.WriteLine("\n1.Validate First name");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UserRegistration userRegistration = new UserRegistration();
+                        Console.WriteLine("Enter First Name");
+                        string fName = Console.ReadLine();
+                        userRegistration.FirstName(fName);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
---
+++ Regex_UserRegistration/Regex_UserRegistration/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_UserRegistration
+{
+    public class UserRegistration
+    {
+        public void FirstName(string fName)
+        {
+            string firstName = "^[A-Z]{1}[a-z]{3,}?$";
+            if(Regex.IsMatch(fName, firstName))
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invallid First Name");
+            }
+        }
+    }
+}
