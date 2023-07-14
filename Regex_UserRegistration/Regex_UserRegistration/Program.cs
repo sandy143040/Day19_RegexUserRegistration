8e94b94a9179ec6930fed2316e6c68b0e4a219f7
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-14 21:55:05 +0530

    [Sandeep] Add : Validate Email Id

--- Regex_UserRegistration/Regex_UserRegistration/Program.cs
+++ Regex_UserRegistration/Regex_UserRegistration/Program.cs
@@ -9,13 +9,38 @@ namespace Regex_UserRegistration
     internal class Program
     {
         static void Main(string[] args)
-        {
-            UserRegistration userRegistration = new UserRegistration();
-            Console.WriteLine("Enter Email id");
-            string input = Console.ReadLine();
-            userRegistration.EmailValidate(input);
-
-            Console.ReadLine();
+        {         
+            while (true)
+            {
+                UserRegistration userRegistration = new UserRegistration();
+                Console.WriteLine("\nPlease choose the below option: ");
+                Console.WriteLine("\n1.Validate First name");
+                Console.WriteLine("2.Validate Last Name");
+                Console.WriteLine("3.Validate Email Id:");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name: ");
+                        string fName = Console.ReadLine();
+                        userRegistration.FirstName(fName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Last Name: ");
+                        string lName = Console.ReadLine();
+                        userRegistration.LastName(lName);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Email id: ");
+                        string emailId = Console.ReadLine();
+                        userRegistration.ValidateEmail(emailId);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
--- Regex_UserRegistration/Regex_UserRegistration/UserRegistration.cs
+++ Regex_UserRegistration/Regex_UserRegistration/UserRegistration.cs
@@ -9,16 +9,40 @@ namespace Regex_UserRegistration
 {
     public class UserRegistration
     {
-        public void EmailValidate(string input)
+        public void FirstName(string fName)
         {
-            string email = "^[a-zA-Z0-9/.]+[@][a-z]+[/.][a-z]{2,3}?$";
-            if(Regex.IsMatch(input, email))
+            string firstName = "^[A-Z]{1}[a-z]{3,}?$";
+            if (Regex.IsMatch(fName, firstName))
             {
-                Console.WriteLine("Valid Email ID");
+                Console.WriteLine("Valid First Name");
             }
             else
             {
-                Console.WriteLine("Invalid Email ID");
+                Console.WriteLine("Invallid First Name");
+            }
+        }
+        public void LastName(string lName)
+        {
+            string lastName = "^[A-Z]{1}[a-z]{3,}?$";
+            if (Regex.IsMatch(lName, lastName))
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name");
+            }
+        }
+        public void ValidateEmail(string emailId)
+        {
+            string email = "^[A-Za-z0-9]+@[a-z]+[.][a-z]?";
+            if (Regex.IsMatch(emailId, email))
+            {
+                Console.WriteLine("Email is Vaild");
+            }
+            else
+            {
+                Console.WriteLine("Email is Invalid");
             }
         }
     }
